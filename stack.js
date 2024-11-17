0d047b3ef67f2f5808161638d5f717d35392317d
Elise-Tea <elise.song.career@gmail.com>
2024-11-17 21:11:30 +0000

    stack problems

--- stack.js
+++ stack.js
@@ -2,32 +2,47 @@
 class Stack {
   constructor() {
     // Initialize an empty array to store stack elements
+    this.stackArray = []
   }
+
 
   isEmpty() {
     // Check if the stack is empty
+    return this.stackArray.length === 0;
   }
 
   // Push element onto the stack
   push(element) {
     // Add the specified element to the end of the items array
+    this.stackArray.push(element);
+    console.log(`Added ${element} to the array.`);
   }
 
   // Pop element from the stack
   pop() {
     // Check if the stack is empty - If empty, return "Underflow"
     // Remove and return the last element of the items array
+    if (!this.isEmpty()) {
+      return this.stackArray.pop();
+    }
+    else {
+      return "Underflow";
+    }
   }
 
   // Peek the top element of the stack
   peek() {
     // Return the last element of the items array without removing it
+    if (!this.isEmpty()) {
+      return this.stackArray[this.stackArray.length - 1];
+    }
   }
 
   // Print the stack elements
   printStack() {
     // Iterate through each element in the items array
     // Return the resulting string with trailing spaces removed
+    return this.stackArray.join("\n");
   }
 }
 
@@ -56,19 +71,33 @@ console.log("Top element:", stack.peek());
 //Problem Statement 2
 function isBalanced(expression) {
   // Initialize an empty array to represent the stack
-
+  const stack = new Stack();
   for (let i = 0; i < expression.length; i++) {
     //Iterate through each character in the expression
     // If it's an opening brace, push it onto the stack
     // If it's a closing brace, check if the stack is empty or if the top of the stack matches the corresponding opening brace
     // If the stack is empty or the top of the stack doesn't match, the expression is unbalanced
+    if (expression[i] === '{' || expression[i] === '(' || expression[i] === '['){
+      stack.push(expression[i]);
+    } else if (expression[i] === '}' || expression[i] === ')' || expression[i] === ']') {
+      if (stack.isEmpty() || !isMatchingPair(stack.pop(),expression[i])){
+        return false;
+      }
+    }
   }
-
   // If the stack is empty at the end, the expression is balanced
+  return stack.isEmpty();
+
 }
 
 function isMatchingPair(opening, closing) {
   // Check if the opening and closing braces form a matching pair
+  const pairs = {
+    "}":"{",
+    "]":"[",
+    ")":"("
+  }
+  return pairs[closing] === opening
 }
 
 // Example usage:
